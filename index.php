4c0396ad9d02fb0545a51c9fb8d3366624310ce5
nakamura1201 <kanriad1201@gmail.com>
2021-01-02 13:34:04 +0900

    1/2編集分

--- index.php
+++ index.php
@@ -5,8 +5,95 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>pimupomuのサイト</title>
+    <link rel="stylesheet" href="<?php echo get_template_directory_uri()?>/assets/css/style.css">
 </head>
 <body>
+    <div class="top-img__wrapper">
+        <img src="<?php echo get_template_directory_uri()?>/assets/img/main-img.png" alt="">
+        <img src="<?php echo get_template_directory_uri()?>/assets/img/title-logo.png" alt="" class="title-logo">
+    </div>
+    <section class="section01">
+        <p>初めまして！pimupomuとしてYouTubeで活動しています！<br>
+            ポケモンの対戦動画や趣味の音楽やプログラミングなどについて発信<br>
+            していきたいと思っています！興味のある方ぜひ仲良くしてください!</p>
+        <div class="plf__wrapper">
+            <div class="plf-box">
+                <div class="plf-box__left">
+                    <ul>
+                        <li><img src="<?php echo get_template_directory_uri()?>/assets/img/slide-img.png" alt=""></li>
+                    </ul>
+                </div>
+                <div class="plf-box__right">
+                    <div class="plf-box__detail">
+                        <img src="" alt="">
+                        <p></p>
+                        <p class="sns-btn"></p>
+                        <p class="sns-btn"></p>
+                        <p class="sns-btn"></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="section02">
+        <div class="main-content__wrapper">
+            <div class="main-content__bg">
+                <div class="main-content__box">
+                    <div class="main-content__left">
+                        <img src="" alt="">
+                        <p></p>
+                    </div>
+                    <div class="main-content__right">
+                        <ul>
+                            <li></li>
+                            <li></li>
+                            <li></li>
+                            <li></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="main-content__wrapper">
+            <div class="main-content__bg">
+                <div class="main-content__box main-content__box__re">
+                    <div class="main-content__left">
+                        <img src="" alt="">
+                        <p></p>
+                    </div>
+                    <div class="main-content__right">
+                        <ul>
+                            <li></li>
+                            <li></li>
+                            <li></li>
+                            <li></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="main-content__wrapper">
+            <div class="main-content__bg">
+                <div class="main-content__box">
+                    <div class="main-content__left">
+                        <img src="" alt="">
+                        <p></p>
+                    </div>
+                    <div class="main-content__right">
+                        <ul>
+                            <li></li>
+                            <li></li>
+                            <li></li>
+                            <li></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <div class="character-bg"></div>
+    <footer>
 
+    </footer>
 </body>
 </html>
