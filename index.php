2ac719a5fe394bd8eca254176e78c9a07d3a07db
nakamura1201 <kanriad1201@gmail.com>
2021-01-16 18:52:17 +0900

    最新ファイル

---
+++ index.php
@@ -0,0 +1,109 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>pimupomuのサイト</title>
+    <link rel="stylesheet" href="<?php echo get_template_directory_uri()?>/assets/css/style.css">
+</head>
+<body>
+    <div class="top-img__wrapper">
+        <video src="<?php echo get_template_directory_uri()?>/assets/video/top-video.mp4" loop autoplay muted></video>
+        <img src="<?php echo get_template_directory_uri()?>/assets/img/title-logo.png" alt="" class="title-logo">
+    </div>
+    <section class="section01">
+        <p class="top-introduction">初めまして！pimupomuとしてYouTubeで活動しています！<br>
+            ポケモンの対戦動画や趣味の音楽やプログラミングなどについて発信<br>
+            していきたいと思っています！興味のある方ぜひ仲良くしてください!</p>
+        <div class="plf__wrapper">
+            <div class="plf-box">
+                <div class="plf-box__left">
+                    <ul>
+                        <li><img src="<?php echo get_template_directory_uri()?>/assets/img/slide-img.png" alt=""></li>
+                    </ul>
+                </div>
+                <div class="plf-box__right">
+                    <div class="plf-box__detail">
+                        <img src="<?php echo get_template_directory_uri()?>/assets/img/section07_.jpg" alt="">
+                        <p>紹介文紹介文紹介文紹介文紹介文紹介文紹介文紹介文紹介文</p>
+                            <div class="sns-btn__wrapper">
+                                <p class="sns-btn"><a class="yo" href="#">youtube</a></p>
+                                <p class="sns-btn"><a class="tw" href="#">twitter</a></p>
+                                <p class="sns-btn"><a class="in" href="#">inst</a></p>
+                            </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="section02">
+        <div class="main-content__wrapper">
+        <h2>GAME</h2>
+            <div class="main-content__bg">
+
+                <div class="main-content__box">
+                    <div class="main-content__left">
+                        <img src="<?php echo get_template_directory_uri()?>/assets/img/section_07.png" alt="">
+                        <p>紹介文紹介文紹介文紹介文紹介文紹介文紹介文紹介文紹介文紹介文紹介文紹介文紹介文紹介文紹介文紹介文紹介文紹介文紹介文紹介文a</p>
+                    </div>
+                    <div class="main-content__right">
+                        <ul>
+                            <li><img src="<?php echo get_template_directory_uri()?>/assets/img/section_07.png" alt=""></li>
+                            <li><img src="<?php echo get_template_directory_uri()?>/assets/img/section_07.png" alt=""></li>
+                            <li><img src="<?php echo get_template_directory_uri()?>/assets/img/section_07.png" alt=""></li>
+                            <li><img src="<?php echo get_template_directory_uri()?>/assets/img/section_07.png" alt=""></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="main-content__wrapper">
+        <h2>MUSIC</h2>
+            <div class="main-content__bg__re">
+
+                <div class="main-content__box main-content__box__re">
+                    <div class="main-content__left">
+                        <img src="<?php echo get_template_directory_uri()?>/assets/img/section_07.png" alt="">
+                        <p>紹介文紹介文紹介文紹介文紹介文紹介文紹介文紹介文紹介文紹介文紹介文紹介文紹介文紹介文紹介文紹介文紹介文紹介文紹介文紹介文a</p>
+                    </div>
+                    <div class="main-content__right">
+                        <ul>
+                            <li><img src="<?php echo get_template_directory_uri()?>/assets/img/section_07.png" alt=""></li>
+                            <li><img src="<?php echo get_template_directory_uri()?>/assets/img/section_07.png" alt=""></li>
+                            <li><img src="<?php echo get_template_directory_uri()?>/assets/img/section_07.png" alt=""></li>
+                            <li><img src="<?php echo get_template_directory_uri()?>/assets/img/section_07.png" alt=""></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="main-content__wrapper">
+        <h2>PC</h2>
+            <div class="main-content__bg">
+
+                <div class="main-content__box">
+                    <div class="main-content__left">
+                        <img src="<?php echo get_template_directory_uri()?>/assets/img/section_07.png" alt="">
+                        <p>紹介文紹介文紹介文紹介文紹介文紹介文紹介文紹介文紹介文紹介文紹介文紹介文紹介文紹介文紹介文紹介文紹介文紹介文紹介文紹介文a</p>
+                    </div>
+                    <div class="main-content__right">
+                        <ul>
+                            <li><img src="<?php echo get_template_directory_uri()?>/assets/img/section_07.png" alt=""></li>
+                            <li><img src="<?php echo get_template_directory_uri()?>/assets/img/section_07.png" alt=""></li>
+                            <li><img src="<?php echo get_template_directory_uri()?>/assets/img/section_07.png" alt=""></li>
+                            <li><img src="<?php echo get_template_directory_uri()?>/assets/img/section_07.png" alt=""></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- <div class="character-bg"></div> -->
+    <footer>
+        <p>Copyright © 2021 pimupomu Inc. All Rights Reserved.</p>
+    </footer>
+    <script src="<?php echo get_template_directory_uri()?>/assets/js/bubbly-bg.js"></script>
+    <script src="<?php echo get_template_directory_uri()?>/assets/js/common.js"></script>
+</body>
+</html>
