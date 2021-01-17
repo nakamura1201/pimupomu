392860b6e67cb79245203aa47788d8ae09bd6537
nakamura1201 <kanriad1201@gmail.com>
2021-01-17 10:49:26 +0900

    確認用コミット

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>pimupomu</title>
+    <title>pimupomu-</title>
     <script src="<?php echo get_template_directory_uri()?>/assets/js/jquery-3.5.1.min.js"></script>
     <link rel="stylesheet" href="<?php echo get_template_directory_uri()?>/assets/css/slick.css"/>
     <link rel="stylesheet" href="<?php echo get_template_directory_uri()?>/assets/css/style.css">
